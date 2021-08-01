fef0d246b68311dffd5c8bf1fe0dfacb6ad60538
Bhabani Samantaray <sankariter@gmail.com>
2021-08-01 17:55:24 +0100

    feat: begin an initial project

---
+++ src/main/java/com/exercise/tech/hills/HillsQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.tech.hills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillsQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HillsQueryServiceApplication.class, args);
+	}
+
+}
